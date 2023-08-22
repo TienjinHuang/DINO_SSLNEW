b2838642e272f95b320f1004bf0fe2175d8acfe0
TienjinHuang <t.huang@tue.nl>
2023-08-22 21:02:44 +0200

    Update train_scripts.sh

--- dino/train_scripts.sh
+++ dino/train_scripts.sh
@@ -1,10 +1,8 @@
 
 #!/user/bin
 
+python -m torch.distributed.launch --nproc_per_node=4 main_dino.py --arch mobilenet --kernel_size 7 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
 
-python -m torch.distributed.launch --nproc_per_node=1 main_dino.py --arch mobilenet --kernel_size 5 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
-python -m torch.distributed.launch --nproc_per_node=1 main_dino.py --arch mobilenet --kernel_size 7 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
+python -m torch.distributed.launch --nproc_per_node=4 main_dino.py --arch mobilenet --kernel_size 9 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
 
-python -m torch.distributed.launch --nproc_per_node=1 main_dino.py --arch mobilenet --kernel_size 9 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
-
-python -m torch.distributed.launch --nproc_per_node=1 main_dino.py --arch mobilenet --kernel_size 11 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
+python -m torch.distributed.launch --nproc_per_node=4 main_dino.py --arch mobilenet --kernel_size 11 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
