4aef088b8f86f1d87858817eafa8b417074e127a
TienjinHuang <t.huang@tue.nl>
2023-08-22 21:45:11 +0200

    Update train_scripts.sh

--- dino/train_scripts.sh
+++ dino/train_scripts.sh
@@ -1,6 +1,9 @@
 
 #!/user/bin
 
+#baseline
+python -m torch.distributed.launch --nproc_per_node=4 main_dino.py --arch mobilenet --kernel_size 5 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
+
 python -m torch.distributed.launch --nproc_per_node=4 main_dino.py --arch mobilenet --kernel_size 7 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
 
 python -m torch.distributed.launch --nproc_per_node=4 main_dino.py --arch mobilenet --kernel_size 9 --data_path /scratch-shared/boqian/imagenet  --output_dir ./
